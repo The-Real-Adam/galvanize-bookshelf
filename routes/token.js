15fee3ee50a19510f4a374f65b6832f3af5fac8b
Adam <Adam@Adams-MacBook-Pro-3.local>
2017-10-02 09:40:10 -0600

    part 4 works

--- routes/token.js
+++ routes/token.js
@@ -1,10 +1,77 @@
-'use strict';
+'use strict'
 
+const bcrypt = require('bcrypt');
+const boom = require('boom');
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const knex = require('../knex');
+const {camelizeKeys} = require('humps');
 
-// eslint-disable-next-line new-cap
-const router = express.Router();
+const JWT_KEY = process.env.JWT_KEY
+const router = express.Router()
+require('dotenv').config
 
-// YOUR CODE HERE
+router.get('/token', (req, res, next) => {
+  jwt.verify(req.cookies.token, JWT_KEY, (err, payload) => {
+    if (err) {
+      res.send(false)
+    } else {
+      res.send(true)
+  }
+  })
+})
 
-module.exports = router;
+router.post('/token', (req, res, next) => {
+  const {email,password} = req.body
+
+  if (!email || !email.trim()) {
+    return next(boom.create(400, 'Email must not be blank'))
+  }
+
+  if (!password || !password.trim()) {
+    return next(boom.create(400, 'Password must not be blank'))
+  }
+
+  knex('users')
+    .where('email', req.body.email)
+    .first()
+    .returning('*')
+    .then((user) => {
+      if (!user) {
+        throw boom.create(400, 'Bad email or password')
+      } else {
+        return bcrypt.compare(req.body.password, user.hashed_password, (err, match) => {
+          if (match) {
+            const token = jwt.sign({userId: user.id}, JWT_KEY)
+
+            let newObj = {
+              id:user.id,
+              email:user.email,
+              firstName: user.first_name,
+              lastName:user.last_name
+            }
+
+            res.cookie('token',token, {httpOnly:true})
+            res.status(200)
+            res.setHeader('Content-Type', 'application/json')
+            res.send(newObj)
+          } else {
+            res.setHeader('Content-Type', 'text/plain')
+            res.status(400)
+            res.send('Bad email or password')
+          }
+        })
+        }
+      // }
+    })
+    .catch((err) => next(err))
+})
+//
+router.delete('/token', (req, res, next) => {
+  res.clearCookie('token')
+  res.sendStatus(200)
+})
+
+
+
+module.exports = router
--- seeds/002_users.js
+++ seeds/002_users.js
@@ -16,7 +16,6 @@ exports.seed = function(knex, Promise) {
         ])
         .then(() => {
           return knex.raw("SELECT setval('users_id_seq',(SELECT MAX(id) FROM users));")
-        })
-
+        }) 
     })
 }
